3741262c5966cee9c1ad47ba6a2d9b07a1d3e6e0
aroj.george <apgeorge@thoughtworks.com>
2012-06-28 22:42:50 +0100

    Basic SPA shell

---
+++ js/spa.js
@@ -0,0 +1,37 @@
+/*jslint    		browser : true, continue : true,       
+devel     : true,  indent :  2,       maxerr    :   50,
+newcap    : true,  nomen  :  true,    plusplus  :   true,
+regexp    : true,  sloppy :  true,    vars      :   true,
+white     : true            
+*/
+
+
+/* global $, spa */
+
+var spa = (function () {
+    var initModule = function () {
+      spa.shell.initModule($('#spa'));
+    };
+    
+    return { initModule : initModule };
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
