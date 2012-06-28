665c97be149a408aa23185baffe3924455ab8635
aroj.george <apgeorge@thoughtworks.com>
2012-06-29 00:49:10 +0100

    Encodes state into uri

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -20,15 +20,27 @@ spa.shell = (function (){
       chat_extend_height  : 450,
       chat_retract_height : 15,
       chat_extended_title : 'Click to retract',
-      chat_retracted_title : 'Click to extend'
+      chat_retracted_title : 'Click to extend',
+      anchor_scheme_map : {
+        chat  : { open : true, closed : true}
+      }
     },
-    stateMap = { is_chat_retracted : true },
+    stateMap = { 
+      anchor_map        : {},
+      is_chat_retracted : true 
+    },
     jqueryMap = {},
     
+    copyAnchorMap, changeAnchorPart, onChangeAddress,
     setJqueryMap, initModule, toggleChat, onClickChat;
     
     //----------------- END MODULE SCOPE VARIABLES ---------------
     //-------------------- BEGIN UTILITY METHODS -----------------
+    
+    // Returns copy of stored anchor map; minimizes overhead
+    copyAnchorMap = function () {
+      return $.extend( true, {}, stateMap.anchor_map );
+    };
     //--------------------- END UTILITY METHODS ------------------
     //--------------------- BEGIN DOM METHODS --------------------
     // Begin DOM method /setJqueryMap/
@@ -103,12 +115,127 @@ spa.shell = (function (){
     };
     // End DOM method /toggleChat/
 
+    // Begin DOM method /changeAnchorPart/
+    // Purpose     : Changes part of the URI anchor component
+    // Arguments : 
+    //     * arg_map - The map describing what part of the URI anchor
+    //    we want changed.
+    // Returns    : boolean
+    //    * true - the Anchor portion of the URI was update
+    //    * false - the Anchor portion of the URI could not be updated
+    // Discussion :
+    //    The current anchor rep stored in stateMap.anchor_map.
+    //    See uriAnchor for a discussion of encoding.
+    //    This method
+    //    * Creates a copy of this map using copyAnchorMap().
+    //    * Modifies the key-values using arg_map.
+    //    * Manages the distinction between independent
+    //    and dependent values in the encoding.
+    //    * Attempts to change the URI using uriAnchor.
+    //    * Returns true on success, and false on failure.
+
+    changeAnchorPart = function ( arg_map ){
+      var
+        anchor_map_revise = copyAnchorMap(),
+        bool_return       = true,
+        key_name, key_name_dep;
+        
+      // Begin merge changes into anchor map
+      KEYVAL:
+      for ( key_name in arg_map ) {
+        if ( arg_map.hasOwnProperty( key_name ) ) {
+          // skip dependent keys during iteration
+          if ( key_name.indexOf( '_' ) === 0 ) { continue KEYVAL; }
+          // update independent key value
+          anchor_map_revise[key_name] = arg_map[key_name];
+          // update matching dependent key
+          key_name_dep = '_' + key_name;
+          if ( arg_map[key_name_dep] ) {
+            anchor_map_revise[key_name_dep] = arg_map[key_name_dep];
+          }
+          else {
+            delete anchor_map_revise[key_name_dep];
+            delete anchor_map_revise['_s' + key_name_dep];
+          }
+        }
+      }
+      
+      // End merge changes into anchor map
+      
+      // Begin attempt to update URI; revert if not successful
+      try {
+        $.uriAnchor.setAnchor( anchor_map_revise );
+      }
+      catch ( error ) {
+        // replace URI with existing state
+        $.uriAnchor.setAnchor( stateMap.anchor_map,null,true );
+        bool_return = false;
+      }
+      // End attempt to update URI...
+      
+      return bool_return;
+      
+    };
     
+    // End DOM method /changeAnchorPart/
     //--------------------- END DOM METHODS ----------------------
     //------------------- BEGIN EVENT HANDLERS -------------------
     
+    // Begin Event handler /onChangeAddress/
+    // Purpose: Handles the address change event
+    // Arguments :
+    //  * event - jQuery event object.
+    // Settings: none
+    // Returns: boolean false
+    // Discussion :
+    //  * Parses the URI anchor component
+    //  * Compares proposed application state with current
+    //  * Adjust the application only where proposed state
+    //    differs from existing
+    //
+    onChangeAddress = function ( event ) {
+      var 
+        anchor_map_previous = copyAnchorMap(),
+        anchor_map_proposed,
+        _s_chat_previous, _s_chat_proposed,
+        s_chat_proposed;
+
+      // attempt to parse anchor
+      try { anchor_map_proposed = $.uriAnchor.makeAnchorMap(); }
+      catch ( error ) {
+        $.uriAnchor.setAnchor( anchor_map_previous, null, true );
+        return false;
+      }
+      stateMap.anchor_map = anchor_map_proposed;                           
+      
+      // convenience vars
+      _s_chat_previous = anchor_map_previous._s_chat;
+      _s_chat_proposed = anchor_map_proposed._s_chat;
+
+      if ( ! anchor_map_previous
+        || _s_chat_previous !== _s_chat_proposed
+      ) {
+          s_chat_proposed = anchor_map_proposed.chat;
+          switch ( s_chat_proposed ) {
+            case 'open' :
+              toggleChat( true );
+              break;
+            case 'closed' :
+              toggleChat( false );
+              break;
+            default :
+              toggleChat( false );
+              delete anchor_map_proposed.chat;
+              $.uriAnchor.setAnchor( anchor_map_proposed, null, true );
+          }
+      }
+      return false;
+    };
+    
     onClickChat = function ( event ){
-      toggleChat ( stateMap.is_chat_retracted );
+      changeAnchorPart({
+          chat : ( stateMap.is_chat_retracted ? 'open' : 'closed')
+      });
       return false;
     };
     
@@ -126,8 +253,17 @@ spa.shell = (function (){
       jqueryMap.$chat
         .attr('title', configMap.chat_retracted_title)
         .click( onClickChat );
+        
+        
+      // configure uriAnchor to use our scheme
+      $.uriAnchor.configModule({
+          schema_map : configMap.anchor_scheme_map
+      });
     };
     
+    // bind address change events
+    // requires the address plugin
+    $.address.change( onChangeAddress );
     
     // test toggle
     // setTimeout ( function () { toggleChat ( true )  }, 3000);
